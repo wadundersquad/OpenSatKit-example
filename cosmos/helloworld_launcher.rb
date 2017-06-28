a0036da9d34eaea541e9d5d0fbee331224a6f555
Johann Miller <jkmiller@umd.edu>
2017-06-28 10:31:37 -0400

    fix paths

--- cosmos/helloworld_launcher.rb
+++ cosmos/helloworld_launcher.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: ascii-8bit
 
-require_relative '../tools/tool_launch'
+require_relative '../../../../tools/tool_launch'
 tool_launch do
-  require 'cosmos/tools/hello_world/hello_world'
+  require_relative 'helloworld'
   Cosmos::HelloWorld.run
 end
